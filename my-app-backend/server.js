3d8f6b49bad0ff172e47e2ea9dbdf8d24d79ec80
janak bohra <janakbohra29@gmail.com>
2020-10-17 19:43:42 +0530

    backend app added

---
+++ my-app-backend/server.js
@@ -0,0 +1,25 @@
+// import 
+var express = require('express');
+var mongoose = require('mongoose');
+var pusher = require('pusher');
+var cors = require('cors');
+
+
+// app config
+const app=express()
+const port=process.env.PORT || 9000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+// db config
+
+// api routes
+app.get('/', (req, res) => res.status(200).send('Hello Janak its working!'))
+
+
+// listening port
+app.listen(port, () => console.log(`listening on localhost:${port}`))
